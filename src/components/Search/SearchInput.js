33b3dca1f42f1dd721c754986801eea3ce318f78
mssq <smiikasiren@gmail.com>
2018-03-02 10:21:12 +0200

    Created component for handling input and button for weather searching

---
+++ src/components/Search/SearchInput.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+
+import SearchImage from '../../img/search.svg';
+
+class SearchInput extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchValue: ''
+    }
+  }
+  
+  render() {
+    const style = {
+      inputStyle: {
+        width: "400px",
+        boxSizing: "border-box",
+        border: "2px solid #ccc",
+        borderRadius: "4px",
+        fontSize: "16px",
+        backgroundImage: `url(${SearchImage})`,
+        backgroundColor: "white",
+        backgroundPosition: "5px 3px",
+        backgroundRepeat: "no-repeat",
+        padding: "12px 20px 12px 40px"
+      },
+      buttonStyle: {
+        width: "70px",
+        fontSize: "16px",
+        padding: "12px 0px 12px 0px",
+        marginLeft: "10px",
+        backgroundColor: "white",
+        boxSizing: "border-box",
+        border: "2px solid #ccc",
+        borderRadius: "4px"
+      }
+    };
+
+    return (
+      <div className="search">
+        <input 
+          style={style.inputStyle}
+          type="text" 
+          placeholder="City name"
+          onChange={event => this.setState({searchValue: event.target.value})}
+          onKeyPress={(e) => {
+            if (e.keyCode === 13 || e.key === 'Enter') {
+              this.props.clicked(this.state.searchValue);
+            }
+          }} 
+        />
+        <button 
+          style={style.buttonStyle}
+          className="search-button"
+          onClick={() => this.props.clicked(this.state.searchValue)}
+        >
+          Search
+        </button>
+      </div>
+    )
+  }
+};
+
+export default SearchInput;
