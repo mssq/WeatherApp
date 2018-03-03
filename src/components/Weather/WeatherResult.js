3781d911419663cd5b09720b26c9f58dc0c4e77a
mssq <smiikasiren@gmail.com>
2018-03-03 16:09:05 +0200

    Added function that converts weather description to weather-icon Added buttons for removing and adding weathers

--- src/components/Weather/WeatherResult.js
+++ src/components/Weather/WeatherResult.js
@@ -1,24 +1,62 @@
 import React, { Component } from 'react';
 
+import '../../css/weather-icons.css';
+
 class WeatherResult extends Component {
 
-  convertKelvin = (kelvin) => {
-    return Math.round( (kelvin - 273.15) * 10) / 10;
+  convertToCelcius = (kelvin) => {
+    return Math.round((kelvin - 273.15));
+  }
+
+  convertWeatherStateToImage = (state) => {
+    switch(state) {
+      case 'Thunderstorm':
+        return <i className="wi wi-thunderstorm weather-state"></i>;
+      case 'Drizzle':
+        return <i className="wi wi-sprinkle weather-state"></i>;
+      case 'Rain':
+        return <i className="wi wi-rain weather-state"></i>;
+      case 'Snow':
+        return <i className="wi wi-snow weather-state"></i>;
+      case 'Mist':
+      case 'Fog':
+        return <i className="wi wi-fog weather-state"></i>;
+      case 'Haze':
+        return <i className="wi wi-day-haze weather-state"></i>;
+      case 'Clear':
+        return <i className="wi wi-day-sunny weather-state"></i>;
+      case 'Clouds':
+        return <i className="wi wi-cloudy weather-state"></i>;
+      case 'Extreme':
+        return <i className="wi wi-tornado weather-state"></i>;
+      default:
+        return <i className="wi wi-cloud weather-state"></i>;
+    }
   }
 
   weatherCard = (weather) => {
     return (
-      <div className="weather-info">
-        <div className="weather-main-data">
-          <h1>{weather.desc}</h1>
-          <h1>{this.convertKelvin(weather.temp)}°C</h1>
+      <div className="weather">
+        <div className="weather-info">
+          <div className="weather-main-data">
+            {this.convertWeatherStateToImage(weather.desc)}
+            <h1>{this.convertToCelcius(weather.temp)}</h1><h2>°C</h2>
+          </div>
+          <div className="weather-country">
+            <h1>{weather.city}, {weather.countryCode}</h1>
+          </div>
+          <div className="weather-secondary-data">
+            <h2>Humidity: {weather.humidity}%</h2>
+            <h2>Wind: {weather.wind} m/s</h2>
+          </div>
         </div>
-        <div className="weather-country">
-          <h1>{weather.city}, {weather.countryCode}</h1>
-        </div>
-        <div className="weather-secondary-data">
-          <h2>Humidity: {weather.humidity}%</h2>
-          <h2>Wind: {weather.wind} m/s</h2>
+        <div className="weather-buttons">
+          <button type="button" className="button-add">
+            ✓
+          </button>
+          <button type="button" className="button-remove">
+            &#x2715;
+          </button>
         </div>
       </div>
     );
