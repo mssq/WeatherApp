1c10b1f90748cb327ed247ee0ba7b98a7a3802ac
mssq <smiikasiren@gmail.com>
2018-03-04 09:58:14 +0200

    Added error message

--- src/reducers/weatherReducer.js
+++ src/reducers/weatherReducer.js
@@ -2,17 +2,18 @@ import { FETCH_WEATHER_START, FETCH_WEATHER_ERROR,
   RECEIVE_WEATHER } from '../constants';
 
 const INITIAL_STATE = {
-  loading: false
+  loading: false,
+  error: 'none'
 }
 
 const weatherReducer = (state=INITIAL_STATE, action) => {
   switch(action.type) {
     case FETCH_WEATHER_START:
       console.log('fetch_weather_start');
-      return { loading: true };
+      return { loading: true, error: 'none' };
     case FETCH_WEATHER_ERROR:
       console.log('fetch_weather_error');
-      return null;
+      return { ...state, loading: false, error: action.payload.message };
     case RECEIVE_WEATHER:
       console.log('receive_weather');
       return {
@@ -23,7 +24,8 @@ const weatherReducer = (state=INITIAL_STATE, action) => {
         city: action.payload.name,
         countryCode: action.payload.sys.country,
         desc: action.payload.weather[0].main,
-        loading: false };
+        loading: false,
+        error: 'none' };
     default:
       console.log('default');
       return null;
