12dcd0908e55aee6e9f525c5884c4363e19af8a7
mssq <smiikasiren@gmail.com>
2018-03-05 19:35:45 +0200

    Added propTypes and defaultProps

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import logo from '../../img/cloud.svg';
 
-const header = ({ title }) => {
-  return (
-    <header className='App-header'>
-      <img src={logo} className='App-logo' alt='logo' />
-      <h1 className='App-title'>{title}</h1>
-    </header>
-  );
-}
+const header = ({ title }) => (
+  <header className="App-header">
+    <img src={logo} className="App-logo" alt="logo" />
+    <h1 className="App-title">{title}</h1>
+  </header>
+);
+
+header.propTypes = {
+  title: PropTypes.string,
+};
+
+header.defaultProps = {
+  title: '',
+};
 
 export default header;
